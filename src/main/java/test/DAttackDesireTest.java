6f14f45df9ddbaeddfc11cf5754816fc209b30fd
IDragonfire <bastian616-git@yahoo.de>
2013-01-19 18:36:13 +0100

    add own Attack Desire

--- src/main/java/test/DAttackDesireTest.java
+++ src/main/java/test/DAttackDesireTest.java
@@ -29,16 +29,14 @@ public class DAttackDesireTest implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Location loc = event.getPlayer().getLocation();
-        loc.setX(loc.getX() + 5);
-        loc.setY(loc.getY() + 1);
+//        loc.setX(loc.getX() + 5);
 
-        RemotePlayer attackEntity;
-        attackEntity = (RemotePlayer) em.createNamedEntity(
+        final RemotePlayer attackEntity = (RemotePlayer) em.createNamedEntity(
                 RemoteEntityType.Human, loc, "attack", false);
         Player p = (Player) attackEntity.getBukkitEntity();
         p.setItemInHand(new ItemStack(Material.IRON_SWORD));
 
-        Mind mind = attackEntity.getMind();
+        final Mind mind = attackEntity.getMind();
         // mind.addMovementDesire(new DesireSwim(attackEntity), 0);
         // mind.addMovementDesire(new DesireAttackOnCollide(attackEntity,
         // EntityHuman.class, false), 2);
@@ -48,16 +46,27 @@ public class DAttackDesireTest implements Listener {
         // mind.addMovementDesire(new DesireLookAtNearest(attackEntity,
         // EntityHuman.class, 8), 7);
         // mind.addMovementDesire(new DesireLookRandomly(attackEntity), 7);
-        DesireFindTarget targetFinderDesire = new DesireFindTarget(
-                attackEntity, 30, false, false);
-        mind.addActionDesire(targetFinderDesire, 1);
-        mind.addActionDesire(new DesireMoveToTarget(attackEntity, 0.0f), 2);
+        // DesireFindTarget targetFinderDesire = new DesireFindTarget(
+        // attackEntity, 30, false, true);
+        // mind.addActionDesire(targetFinderDesire, 1);
+        // mind.addMovementDesire(new DesireMoveToTarget(attackEntity, 0.0f), 2);
         // mind.addActionDesire(new DesireAttackNearest(attackEntity,
         // EntityHuman.class, 16, false, true, 0), 2);
+        Bukkit.getScheduler().scheduleSyncDelayedTask(
+                RemoteEntities.getInstance(), new Runnable() {
+
+                    @Override
+                    public void run() {
+                        mind.addActionDesire(new DAttackEntity(attackEntity,
+                                Bukkit.getOnlinePlayers()[0]), 0);
+                        System.out.println("add mind");
+                       
+                    }
+                }, 20 * 5);
 
         // loc.setX(loc.getX() + 3);
         // em.createNamedEntity(RemoteEntityType.Human, loc, "test", false);
-        Bukkit.broadcastMessage("npc spawn");
+        Bukkit.broadcastMessage("npc spawn 2");
     }
 
     @EventHandler
