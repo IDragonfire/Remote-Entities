95560e47edd05ef3f4ccd87c95e11040d6459ce7
IDragonfire <bastian616-git@yahoo.de>
2013-01-19 14:16:29 +0100

    fix compile errors in examples

--- examples/TameOnJoin/ExampleMain.java
+++ examples/TameOnJoin/ExampleMain.java
@@ -1,11 +1,11 @@
-package de.kumpelblase2.remoteentities.examples;
+package TameOnJoin;
 
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
 import de.kumpelblase2.remoteentities.EntityManager;
 import de.kumpelblase2.remoteentities.RemoteEntities;
 import de.kumpelblase2.remoteentities.api.RemoteEntity;
@@ -13,24 +13,32 @@ import de.kumpelblase2.remoteentities.api.RemoteEntityType;
 import de.kumpelblase2.remoteentities.api.features.RemoteTamingFeature;
 import de.kumpelblase2.remoteentities.api.features.TamingFeature;
 import de.kumpelblase2.remoteentities.api.thinking.goals.DesireFollowTamer;
+import de.kumpelblase2.remoteentities.exceptions.PluginNotEnabledException;
 
-public class ExampleMain extends JavaPlugin implements Listener
-{
-	private EntityManager npcManager;
-	
-	public void onEnable()
-	{
-		Bukkit.getPluginManager().registerEvents(this, this);
-		this.npcManager = RemoteEntities.createManager(this);
-	}
-	
-	@EventHandler
-	public void onJoin(PlayerJoinEvent inEvent) throws Exception
-	{
-		RemoteEntity entity = npcManager.createNamedEntity(RemoteEntityType.Human, inEvent.getPlayer().getLocation(), "test");
-		TamingFeature feature = new RemoteTamingFeature(entity);
-		feature.tame(inEvent.getPlayer());
-		entity.getFeatures().addFeature(feature);
-		entity.getMind().addMovementDesire(new DesireFollowTamer(entity, 5, 15), entity.getMind().getHighestMovementPriority() + 1);
-	}
+public class ExampleMain extends JavaPlugin implements Listener {
+    private EntityManager npcManager;
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+        try {
+            this.npcManager = RemoteEntities.createManager(this);
+        } catch (PluginNotEnabledException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent inEvent) throws Exception {
+        RemoteEntity entity = this.npcManager.createNamedEntity(
+                RemoteEntityType.Human, inEvent.getPlayer().getLocation(),
+                "test");
+        TamingFeature feature = new RemoteTamingFeature(entity);
+        feature.tame(inEvent.getPlayer());
+        entity.getFeatures().addFeature(feature);
+        entity.getMind().addMovementDesire(
+                new DesireFollowTamer(entity, 5, 15),
+                entity.getMind().getHighestMovementPriority() + 1);
+    }
 }
